cb43a23219c53e907b196af71aeb363ff7a84696
arielibarbalz <ibarbalz@gmail.com>
2025-05-05 21:23:24 -0300

    coin

--- coin.sol
+++ coin.sol
@@ -1,23 +1,29 @@
-// SPDX-License-Identifier: MIT
-pragma solidity  >=0.8.17 <0.9.0 ;
+pragma solidity >=0.8.17 <0.9.0;
 
-contract coin{
+contract Coin {
     address public minter;
-    mapping (address => uint) public balances;
-
+    mapping(address => uint) public balances;
+    
     event Sent(address from, address to, uint amount);
-
+    
     constructor() {
         minter = msg.sender;
     }
+    
     function mint(address receiver, uint amount) public {
-        require(msg.sender == minter);
+        require(msg.sender == minter, "Only minter can mint");
         balances[receiver] += amount;
     }
-    error InsuficientBalance(uint requested, uint available);
     
-    function send(address receiver, uint amount) public{
-        require(amount <= balances[msg.sender], InsuficientBalance(amount, balances[msg.sender]));
-        balances
+    error InsufficientBalance(uint requested, uint available);
+    
+    function send(address receiver, uint amount) public {
+        if (amount > balances[msg.sender]) {
+            revert InsufficientBalance(amount, balances[msg.sender]);
+        }
+        
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
     }
 }
